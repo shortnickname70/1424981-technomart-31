d23adfd96e39166a044e3d2632aac8a8f658ad99
shortnickname70 <78146419+shortnickname70@users.noreply.github.com>
2021-07-02 15:50:46 +0700

    фокус и js
    -исправила фокусы у Закладок и Корзины на главной странице
    -поправила слайдер, теперь переключается
    -добавила форму js по образу и подобию того, что делали на лайве, но она не появляется, понятия не имею почему

---
+++ login.js
@@ -0,0 +1,55 @@
+const getlostLink = document.querySelector(".get-lost");
+const getlostPopup = document.querySelector(".modal");
+const getlostClose = getlostPopup.querySelector(".feedback-modal-close-button");
+const getlostForm = getlostPopup.querySelector(".login-form");
+const getlostName = getlostPopup.querySelector(".login-name-icon");
+const getlostEmail = getlostPopup.querySelector(".login-email-icon");
+
+let isStorageSupport = true;
+let storage = "";
+
+try {
+  storage = localStorage.getItem("login");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+getlostLink.addEventListener("click", function () {
+ evt.preventDefault();
+ getlostPopup.classList.add("modal-show");
+
+ if (storage) {
+    getlostName.value = storage;
+    getlostEmail.focus();
+} else {
+    getlostName.focus();
+  }
+});
+
+getlostClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    getlostPopup.classList.remove("modal-show");
+    getlostPopup.classList.remove("modal-error");
+  });
+
+  getlostForm.addEventListener("submit", function (evt) {
+    if (!getlostName.value || !getlostEmail.value){
+    evt.preventDefault();
+    getlostPopup.classList.remove("modal-error");
+    getlostPopup.offsetWidth = getlostPopup.offsetWidth;
+    getlostPopup.classList.add("modal-error");
+}else {
+    if (isStorageSupport) {
+    localStorage.setItem("login", getlostName.value);
+}
+}
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (getlostPopup.classList.contains("modal-show")) {
+        evt.preventDefault();
+        getlostPopup.classList.remove("modal-show");
+      }
+    }
+  });
